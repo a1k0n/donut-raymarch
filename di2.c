9f7fab981b94c7f9f77df93ace0a0b3ff0b67763
Andy Sloane <andy@a1k0n.net>
2023-10-21 21:43:46 -0700

    magic circle rotation

--- di2.c
+++ di2.c
@@ -1,16 +1,18 @@
+#include <stdint.h>
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
 #include <math.h>
 
-#define R(t,x,y) _t=x;x-=t*y;y+=t*_t;_t=(3-x*x-y*y)/2;x*=_t;y*=_t;
+//#define R(t,x,y) _t=x;x-=t*y;y+=t*_t;_t=(3-x*x-y*y)/2;x*=_t;y*=_t;
+#define R(s,x,y) x-=(y>>s);y+=(x>>s)
 const float dz = 5, r1 = 1, r2 = 2;
 
-// CORDIC algorithm to find magnitude of |x,y| in 8.8 fixed point, effectively multiplied by 0.607
-int li(int x, int y) {
+// CORDIC algorithm to find magnitude of |x,y| in 8.8 fixed point
+int length_cordic(int16_t x, int16_t y) {
   if (x<0) x=-x;
   //printf("%d %d (%f) ->", x, y, sqrt((float)x*x + (float)y*y));
-  for (int i = 0; i < 10; i++) {
+  for (int i = 0; i < 8; i++) {
     int t=x;
     if (y<0) {
       x-=y>>i;
@@ -21,14 +23,20 @@ int li(int x, int y) {
     }
   }
   //printf(" %d %d -> %d\n", x, y, x * 155 >> 8);
-  return x; // * 155 >> 8; // correct for the 0.607 factor
+   // * 155 >> 8; // correct for the 0.607 factor
+  return (x >> 1) + (x >> 3); // divide by 0.625 as a cheap approximation to the 0.607 factor
 }
 
 void main() {
+  /*
   float sA = 0, cA = 1,  // sines and cosines of
         sB = 0, cB = 1,  // donut rotation angles A and B
-        _t;
+        */
+  int16_t sBf = 0, cBf = 16384;
+  int16_t sAf = 11583, cAf = 11583;
   for (;;) {
+    float sA = sAf/16384.0, cA = cAf/16384.0;
+    float sB = sBf/16384.0, cB = cBf/16384.0;
     float x0 = cB*sA,
           x1 = cA*cB,
           p0x = dz*sB,
@@ -41,6 +49,9 @@ void main() {
     int lyi = 256*ly;
     int lzi = 256*lz;
 
+    const int r1i = r1*256;
+    const int r2i = r2*256;
+
     int niters = 0;
     int nnormals = 0;
     for (int j = 0; j < 23; j++) {
@@ -51,7 +62,7 @@ void main() {
               vx = cB*x - sB,
               vy = cA*y - sA*x2 - x0,
               vz = cA*x2 + sA*y + x1;
-        int t = (int) (256 * (dz - r2 - r1));
+        int t = (int) (256 * dz) - r2i - r1i;
 
         int px = (int)((p0x*256 + t * vx));
         int py = (int)((p0y*256 + t * vy));
@@ -61,10 +72,10 @@ void main() {
         int vzi = (int)(vz*256);
         for (;;) {
           int t0, t1, t2, d;
-          t0 = li(px, py) * 155 >> 8;
-          t1 = t0 - 256*r2;
-          t2 = li(pz, t1) * 155 >> 8;
-          d = t2 - r1*256;
+          t0 = length_cordic(px, py);
+          t1 = t0 - r2i;
+          t2 = length_cordic(pz, t1);
+          d = t2 - r1i;
           //printf("[%d,%d] (pz, t1) = (%d, %d) -> %d, d=%d, t0=%d\n", i, j, pz, t1, t2, d, t);
           t += d;
           //printf("%d %d %d -%d (%0.3f, %0.3f, %0.3f)-> ", px, py, pz, d, vx, vy, vz);
@@ -92,8 +103,8 @@ void main() {
     }
     printf("%d iterations %d lit pixels", niters, nnormals);
     fflush(stdout);
-    R(.03, cA, sA);
-    R(.011, cB, sB);
+    R(5, cAf, sAf);
+    R(6, cBf, sBf);
     usleep(15000);
     printf("\r\x1b[23A");
   }
